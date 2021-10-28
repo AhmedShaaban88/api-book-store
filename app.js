0408e6a949704a0f49f204c0ea772ba9414542fa
Ahmed_Shaaban <ahmedshaaban3288@gmail.com>
2021-10-28 12:48:39 +0200

    add rate limit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const helmet = require("helmet");
 const catchError = require('http-errors');
 const compression = require('compression');
+const rateLimit = require("express-rate-limit");
 const errorHandler = require('./middlewares/errorHandler');
 const langInterceptor = require('./middlewares/langInterceptor');
 const publicRoutes = require('./routes/public');
@@ -17,9 +18,12 @@ app.use(compression())
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(langInterceptor);
-
-
-app.use(`/api${process.env.api_version}`, publicRoutes);
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 15,
+    message: "Too many accounts created from this IP, please try again after an 15 Minutes"
+});
+app.use(`/api${process.env.api_version}`,limiter, publicRoutes);
 app.use(`/api${process.env.api_version}/auth`, protectedRoutes);
 app.use(function (req, res, next) {
     next(catchError.NotFound());
